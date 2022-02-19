44c64e50858e2f8370067835dac4404f245292a1
Medet Mussayev <acostyle.m.z@protonmail.com>
2022-02-19 18:59:47 +0600

    main: add rocket animation

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import time
 import curses
 import random
 import asyncio
+import os
+
+from curses_tools import draw_frame
 
 
 TIC_TIMEOUT = 0.1
@@ -39,6 +42,20 @@ async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0
         column += columns_speed
 
 
+async def animate_spaceship(canvas, row, column, *frames):
+    """Display animation of rocket."""
+    draw_frame(canvas, row, column, frames[0])
+    while True:
+        draw_frame(canvas, row, column, frames[0], negative=True)
+        draw_frame(canvas, row, column, frames[1])
+        for _ in range(3):
+            await asyncio.sleep(0)
+        draw_frame(canvas, row, column, frames[1], negative=True)
+        draw_frame(canvas, row, column, frames[0])
+        for _ in range(3):
+            await asyncio.sleep(0)
+
+
 async def blink(canvas, row, column, symbol='*'):
     """Display animation of stars."""
 
@@ -69,10 +86,22 @@ def get_random_coordinates(max_row, max_column):
     return random.randint(1, max_row-2), random.randint(1, max_column-2)
 
 
+def get_frames_from_files(frames_dir):
+    """Get frames."""
+    frames = []
+    for frame_file in os.listdir(f'{frames_dir}'):
+        with open(f'{frames_dir}/{frame_file}') as _file:
+            frames.append(_file.read())
+
+    return frames
+
+
 def draw(canvas):
     """Draw game."""
-
     max_row, max_column = canvas.getmaxyx()
+
+    curses.curs_set(False)
+    canvas.border()
 
     coroutines = [
         blink(
@@ -82,7 +111,10 @@ def draw(canvas):
         )
         for _ in range(STARS_AMOUNT)
     ]
-    coroutines.append(fire(canvas, max_row-2, max_column//2))
+    coroutines.append(fire(canvas, max_row//2, max_column//2 + 2))
+    
+    rocket_frames = get_frames_from_files('rocket_frames')
+    coroutines.append(animate_spaceship(canvas, max_row//2, max_column//2, *rocket_frames))
 
     while True:
         for coroutine in coroutines:
@@ -98,7 +130,5 @@ def draw(canvas):
 
 
 if __name__ == '__main__':
-    curses.initscr()
     curses.update_lines_cols()
-    curses.curs_set(False)
     curses.wrapper(draw)
