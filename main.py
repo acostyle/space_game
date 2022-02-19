cf2fd67e997d213e92a71079668f3f669845cdd8
Medet Mussayev <acostyle.m.z@protonmail.com>
2022-02-19 18:36:21 +0600

    main: add fire animation

--- main.py
+++ main.py
@@ -4,39 +4,97 @@ import random
 import asyncio
 
 
+TIC_TIMEOUT = 0.1
+SYMBOLS = '+*.:'
+STARS_AMOUNT = 100
+
+
+async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
+    """Display animation of gun shot. Direction and speed can be specified."""
+
+    row, column = start_row, start_column
+
+    canvas.addstr(round(row), round(column), '*')
+    await asyncio.sleep(0)
+
+    canvas.addstr(round(row), round(column), 'O')
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), ' ')
+
+    row += rows_speed
+    column += columns_speed
+
+    symbol = '-' if columns_speed else '|'
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), ' ')
+        row += rows_speed
+        column += columns_speed
+
+
 async def blink(canvas, row, column, symbol='*'):
+    """Display animation of stars."""
+
+    for _ in range(random.randint(0, 10)):
+        await asyncio.sleep(0)
+
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        await asyncio.sleep(0)
+        for _ in range(20):
+            await asyncio.sleep(0)
 
-        time.sleep(0.01)
         canvas.addstr(row, column, symbol)
-        await asyncio.sleep(0)
+        for _ in range(3):
+            await asyncio.sleep(0)
 
-        time.sleep(0.01)
         canvas.addstr(row, column, symbol, curses.A_BOLD)
-        await asyncio.sleep(0)
+        for _ in range(5):
+            await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        await asyncio.sleep(0)
+        for _ in range(3):
+            await asyncio.sleep(0)
+
+
+def get_random_coordinates(max_row, max_column):
+    """Get random star coordinates."""
+
+    return random.randint(1, max_row-2), random.randint(1, max_column-2)
 
 
 def draw(canvas):
-    coroutines = []
+    """Draw game."""
+
     max_row, max_column = canvas.getmaxyx()
 
-    for _ in range(1, 100):
-        row = random.randint(1, max_row-1)
-        column = random.randint(1, max_column-1)
-        symbol = random.choice('+*.:')
-        coroutine = blink(canvas, row=row, column=column, symbol=symbol)
-        coroutines.append(coroutine)
+    coroutines = [
+        blink(
+            canvas,
+            *get_random_coordinates(max_row, max_column),
+            random.choice(SYMBOLS)
+        )
+        for _ in range(STARS_AMOUNT)
+    ]
+    coroutines.append(fire(canvas, max_row-2, max_column//2))
 
     while True:
-        coroutine = random.choice(coroutines)
-        coroutine.send(None)
-        canvas.border()
+        for coroutine in coroutines:
+            try:
+                coroutine.send(None)
+            except StopIteration:
+                coroutines.remove(coroutine)
+        if len(coroutines) == 0:
+            break
+
         canvas.refresh()
+        time.sleep(TIC_TIMEOUT)
 
 
 if __name__ == '__main__':
